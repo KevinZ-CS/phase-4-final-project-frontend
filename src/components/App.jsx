200028eac247c12e0c81144dde9388377f41af11
Kevin Zheng <kevzheng27@gmail.com>
2022-12-16 18:41:10 -0500

    Built out NavBar component with jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import NavBar from './NavBar';
 
 
 
+
 function App() {
   return (
     <>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+// import { NavLink } from 'react-router-dom';
 
 function NavBar() {
     return (
-        <h1>Hello World</h1>
+        <>
+        <Navbar className="navbar-light bg-white fixed-top" expand='lg' >
+
+        <Container>
+        <Navbar.Brand href="/">Game World</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Login</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+
+        </Navbar>
+        </>
     )
 }
 
