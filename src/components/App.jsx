9ab78d7bc195640915a2f8c2dda9bd49c70b7235
Kevin Zheng <kevzheng27@gmail.com>
2022-12-16 13:37:11 -0500

    Delete not needed files and set up index files and component folder

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import { Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <Switch>
+      <Route exact path = '/'>
+        
+      </Route>
+    </Switch>
+  );
+}
+
+export default App;
