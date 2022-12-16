c50c0d1af024b6053d3e3696b89a208ef8e721df
Kevin Zheng <kevzheng27@gmail.com>
2022-12-16 18:18:03 -0500

    Create NavBar component file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,19 @@
 import { Route, Switch } from 'react-router-dom';
+import NavBar from './NavBar';
+
+
 
 function App() {
   return (
+    <>
+    <NavBar />
     <Switch>
+
       <Route exact path = '/'>
-        
+
       </Route>
     </Switch>
+    </>
   );
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
+function NavBar() {
+    return (
+        <h1>Hello World</h1>
+    )
+}
+
+export default NavBar
