2ca17c2c2ebdc75e9aff8ca05a843a1b3a4f9c87
Kevin Zheng <kevzheng27@gmail.com>
2022-12-17 11:51:07 -0500

    Create GameList component file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,19 +4,20 @@ import { Route, Switch } from 'react-router-dom';
 import NavBar from './NavBar';
 import LoginForm from './LoginForm';
 import SignUpForm from './SignUpForm';
+import GameList from './GameList';
 
 
 function App() {
   const [user, setUser] = useState(null);
 
-// useEffect(() => {
-//   fetch('/users')
-//   .then((response) => {
-//     if(response.ok) {
-//       response.json().then((user) => setUser(user));
-//     }
-//   });
-// }, [])
+useEffect(() => {
+  fetch('/users')
+  .then((response) => {
+    if(response.ok) {
+      response.json().then((user) => setUser(user));
+    }
+  });
+}, [])
 
   return (
     <>
@@ -24,6 +25,7 @@ function App() {
     <Switch>
 
       <Route exact path = '/'>
+        <GameList />
       </Route>
 
       <Route exact path = '/login'>
---
+++ src/components/GameList.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row } from 'react-bootstrap'
+import Item from "./Item";
+
+function GameList() {
+    return (
+        <>
+        <Container>
+
+        </Container>
+        </>
+    )
+}
+
+export default GameList
