2771825a8879e72c652c2182fe7c82b2ede59d23
Kevin Zheng <kevzheng27@gmail.com>
2022-12-16 19:31:55 -0500

    Built out layout of LoginForm component with JSX

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,12 +1,68 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Form, Container } from 'react-bootstrap';
+import { Form, Button, Container, Row } from 'react-bootstrap';
 
 function LoginForm() {
+
     return(
-        <>
-        <h1>Hello</h1>
-        </>
+     <>
+    <Container className="successmargins">
+     
+        <Row>
+        <div className="col-md-4"></div>
+    <Form className="col-md-4">
+
+{/* <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p> */}
+  <Form.Group className="mb-2" controlId="exampleForm.ControlInput1">
+       <Form.Control
+          type="text"
+          placeholder="Username"
+          autoFocus
+          className="shadow-none login-input"
+          autoComplete="off"
+        //   onChange={(e) => setEmail(e.target.value)}
+        //   value={email}
+          required
+      />
+  </Form.Group>
+
+
+
+
+  <Form.Group className="mb-0" controlId="exampleForm.ControlInput2">
+      <Form.Control
+          type="password"
+          placeholder="Password"
+          className="shadow-none login-input"
+          autoComplete="off"
+        //   onChange={(e) => setPwd(e.target.value)}
+        //   value={pwd}
+          required
+      />
+  </Form.Group>
+
+
+{/* <div className="text-center mb-4 mt-4">
+  <a id='pw' href="#">Forgot Password?</a>
+</div> */}
+
+<hr className="mt-0" />
+
+
+
+<div id='login' className="text-center mb-3">
+  <button className="btn btn-dark" type="submit" >Sign In</button>
+</div>
+
+<div id="acc" className="text-center mb-3">
+<span>New to Game World? </span>
+{/* <NavLink to='/signup' className='no-style' onClick={handleClose}>Create Account</NavLink> */}
+</div>
+</Form>
+</Row>
+    </Container>
+     
+    </>
     )
 }
 
