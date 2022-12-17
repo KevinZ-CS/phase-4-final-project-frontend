121292ccafbee52f77ee30b0d76b861dc570ee86
Kevin Zheng <kevzheng27@gmail.com>
2022-12-17 11:58:03 -0500

    Built out GameList component layout using JSX

--- src/components/GameList.jsx
+++ src/components/GameList.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row } from 'react-bootstrap'
-import Item from "./Item";
+
 
 function GameList() {
     return (
         <>
-        <Container>
+        <Container className='py-5 mt-5 mb-5'>
+            <div className="title text-center">
+                <h2 className="position-relative d-inline-block">All Games</h2>
+            </div>
+        <Row>
 
+        </Row>
         </Container>
         </>
     )
